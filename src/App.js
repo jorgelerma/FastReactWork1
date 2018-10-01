1e5c6e9682c5d415c5bfc415bbb0d6f1c014a72a
Jorge Lerma <33969997+jorgelerma@users.noreply.github.com>
2018-09-30 21:26:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends React.Component {
             <button type="button" onClick={this.handleRemoveShareholder(idx)} className="small">-</button>
           </div>
         ))}
-        <button type="button" onClick={this.handleAddShareholder} className="small">Agregar au Platilloz</button>
+        <button type="button" onClick={this.handleAddShareholder} className="small">Agregar au the Platilloz</button>
         <button>Incorporate</button>
 
         <button type="button" onClick={this.showContents.bind(this)}>Show Content</button>
