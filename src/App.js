b44c8f4267f6e108a73a3ada003eab9d65e74ea1
jorgel <jorge@jorgelerma.com>
2018-09-30 19:29:09 -0500

    "Rename button1 part2"

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends React.Component {
             <button type="button" onClick={this.handleRemoveShareholder(idx)} className="small">-</button>
           </div>
         ))}
-        <button type="button" onClick={this.handleAddShareholder} className="small">Agregar Platilloz</button>
+        <button type="button" onClick={this.handleAddShareholder} className="small">Agregar un Platilloz</button>
         <button>Incorporate</button>
 
         <button type="button" onClick={this.showContents.bind(this)}>Show Content</button>
